f9ffcc00194d4f3ea757ff2f4ea257e15ffeec24
melchavezg <mchavezg300@gmail.com>
2023-09-01 10:53:54 -0700

    combining functions

--- practice.js
+++ practice.js
@@ -1,4 +1,22 @@
 //quoting strings with single quote strings
 //ex1.
     constmyStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
-javascript gi
+//combining functions
+    function isEven(x) {
+        if (x % 2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    console.log(isEven(10));
+    console.log(isEven(9));
+
+    let number = 10;
+    if (isEven(10)) {
+        console.log(number + ' is even');
+} else {
+        console.log(number + ' not even');
+    }
+
+
