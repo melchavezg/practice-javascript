7b8c0cd85f3d753a2d61709710a8cee3169a9555
melchavezg <mchavezg300@gmail.com>
2023-09-28 21:58:09 -0500

    nesting arrays

--- practice.js
+++ practice.js
@@ -93,4 +93,6 @@ const thirdLetterOfLastName = lastName[2];
 const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName;
+const lastLetterOfLastName = lastName[lastName.length - 1];
+
+const myArray = [["Lakers", 50],["Bulls", 76]];
