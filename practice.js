cfa58a9336d5eedf3fad2b4f15d947a15ddbd7e5
melchavezg <mchavezg300@gmail.com>
2023-10-11 23:44:19 -0500

    logical order

--- practice.js
+++ practice.js
@@ -299,4 +299,17 @@ function testElseIf(val) {
 }
 
 testElseIf(7);
-new
+
+//Logical Order in If Else Statements
+
+function orderMyLogic(val) {
+    if (val < 10) {
+        return "Less than 10";
+    } else if (val < 5) {
+        return "Less than 5";
+    } else {
+        return "Greater than or equal to 10";
+    }
+}
+
+orderMyLogic(7);
