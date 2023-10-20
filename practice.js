9a91369d065561e76df95feb8ff3009cbc9cc15c
melchavezg <mchavezg300@gmail.com>
2023-10-19 22:06:01 -0500

    chaining

--- practice.js
+++ practice.js
@@ -326,3 +326,14 @@ function orderMyLogic(val) {
 }
 
 orderMyLogic(7);
+
+//Chaining If Else Statements
+function testSize(num) {
+    // Only change code below this line
+
+
+    return "Change Me";
+    // Only change code above this line
+}
+
+testSize(7);
