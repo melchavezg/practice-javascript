b7e5842db71ff267660a73945ba9cbe838b122f5
melchavezg <mchavezg300@gmail.com>
2023-10-24 23:54:49 -0500

    continue golf

--- practice.js
+++ practice.js
@@ -349,11 +349,12 @@ testSize(7);
 const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
 function golfScore(par, strokes) {
-    // Only change code below this line
+    if (par > strokes) {
+        return ""
+    }
 
 
     return "Change Me";
-    // Only change code above this line
 }
 
 golfScore(5, 4);
